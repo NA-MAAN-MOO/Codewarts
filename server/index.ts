6938ab2afef470b0789fdb89e6fe388f1c79b584
김세희 <sadie1001@naver.com>
2023-02-05 16:37:17 +0900

    chore : 서버에 mongoose 연동 mongodb 라이브러리 설치 mongoose 라이브러리 설치 gitignore 수정 mongoose 서버 연동

--- server/index.ts
+++ server/index.ts
@@ -1,20 +1,28 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import mongoose from "mongoose";
 
+//환경변수 이용
 dotenv.config();
+const port = process.env.PORT;
+const mongoPassword = process.env.MONGO_PW;
 
 const app: Express = express();
-const port = process.env.PORT;
 
+app.use(cors());
 app.use(express.static("../client/build"));
 app.use(express.json());
-app.use(cors());
+const db = `mongodb+srv://juncheol:${mongoPassword}@cluster0.v0izvl3.mongodb.net/?retryWrites=true&w=majority`;
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
 
 // app.get("/", (req: Request, res: Response) => {
 //  res.sendFile("../client/build/index.html");
 // });
 
-app.listen(8080, () => {
+app.listen(port, () => {
   console.log("server is running");
 });
