6413bacb8b40c0be7dcd7c4751d70f7f93a13326
김세희 <sadie1001@naver.com>
2023-02-09 01:11:43 +0900

    feat : Set redux-toolkit slice 파일 작성 index에 추가

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { store } from './stores';
+import { Provider } from 'react-redux';
+import './styles/Reset.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
