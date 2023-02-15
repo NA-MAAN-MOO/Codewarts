602e0cacd0df12e673129022aa0b91b94145891d
김세희 <sadie1001@naver.com>
2023-02-16 01:17:12 +0900

    style : Delete useless annotations

--- client/src/pages/Start.tsx
+++ client/src/pages/Start.tsx
@@ -1,29 +1,7 @@
-import React, { ChangeEvent, useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { openLobby } from '../stores/modeSlice';
 import styled from 'styled-components';
-import { RootState } from '../stores';
-import SelectBox from 'objects/SelectBox';
 import LoginDialog from './LoginDialog';
-import main_background from 'assets/images/main_background.png';
 
 const Start = () => {
-  const dispatch = useDispatch();
-  const [nameInput, setNameInput] = useState('');
-  const { userName } = useSelector((state: RootState) => state.editor);
-  const [charactorIamge, setCharatorIamge] = useState('char1');
-  const CHARACTORMODELS = 28;
-
-  function onNameChange(e: ChangeEvent<HTMLInputElement>) {
-    const newName = e.target.value;
-    setNameInput(newName);
-  }
-
-  function onChangeModel(e: ChangeEvent<HTMLOptionElement>) {
-    const newCharactorIamge = e.target.value;
-    setCharatorIamge(newCharactorIamge);
-  }
-
   return (
     <StartDiv>
       <LoginDialog />
@@ -41,16 +19,4 @@ const StartDiv = styled.div`
   justify-content: center;
   flex-direction: column;
   gap: 50px;
-`;
-
-const LogoDiv = styled.div`
-  font-size: 80px;
-`;
-
-const LoginBtn = styled.button`
-  font-size: 40px;
-  padding: 20px;
-  background-color: tomato;
-  border: none;
-  border-radius: 20px;
 `;
