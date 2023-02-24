6ae87f1c45149131d41ebff4072def36c50a6d92
HyejiYeom <kristi8041@gmail.com>
2023-02-25 01:06:00 +0900

    feat : Add transition from lobby to main

--- client/src/scenes/Mainscene.tsx
+++ client/src/scenes/Mainscene.tsx
@@ -56,6 +56,13 @@ export default class MainScene extends Phaser.Scene {
   }
 
   create() {
+    /* Transition */
+    this.cameras.main.fadeFrom(
+      1200,
+      Phaser.Math.Between(50, 255),
+      Phaser.Math.Between(50, 255),
+      Phaser.Math.Between(50, 255)
+    );
     const newFriendSoundToggle = SoundPlayer(friendSoundFile);
     // this.getOut = false;
     this.openMyEditor = false;
@@ -384,21 +391,24 @@ export default class MainScene extends Phaser.Scene {
       if (!this.watchTable) return;
 
       /*--------------에디터 리스트 보여주는 부분--------------- */
-      for (let i = 0; i < 5; i++) {
-        if (i === this.editorIdx) {
-          this.tableMap
-            .get(this.player.touching[0].body.id)
-            ?.editorBtnList[i].setStyle({
-              backgroundColor: 'white',
-              // color: 'white',
-            });
-        } else {
-          this.tableMap
-            .get(this.player.touching[0].body.id)
-            ?.editorBtnList[i].setStyle({
-              backgroundColor: 'transparent',
-              // color: '#ff6f00',
-            });
+      // FIXME: body 못 읽는 부분, 일단은 touching에 무언가가 있을 때만 그려주도록 함
+      if (this.player.touching.length !== 0) {
+        for (let i = 0; i < 5; i++) {
+          if (i === this.editorIdx) {
+            this.tableMap
+              .get(this.player.touching[0].body.id)
+              ?.editorBtnList[i].setStyle({
+                backgroundColor: 'white',
+                // color: 'white',
+              });
+          } else {
+            this.tableMap
+              .get(this.player.touching[0].body.id)
+              ?.editorBtnList[i].setStyle({
+                backgroundColor: 'transparent',
+                // color: '#ff6f00',
+              });
+          }
         }
       }
 
