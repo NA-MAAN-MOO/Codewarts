72a69cd4cc6219cc136cad291e898bcc1ef8bf63
HyejiYeom <kristi8041@gmail.com>
2023-02-27 16:49:42 +0900

    refactor : Use mongoose to get boj ranking

--- server/controllers/whiteboardController.ts
+++ server/controllers/whiteboardController.ts
@@ -1,12 +1,15 @@
 /* Use env variable */
-import dotenv from 'dotenv';
-dotenv.config();
+// import dotenv from 'dotenv';
+// dotenv.config();
 
 import { Request, Response } from 'express';
 
-const { MongoClient } = require('mongodb');
+// const { MongoClient } = require('mongodb');
 import { ObjectId } from 'mongoose';
-const mongoPassword = process.env.MONGO_PW;
+// const mongoPassword = process.env.MONGO_PW;
+// import { mongoClient } from '../servers/dbServer';
+import User from '../models/User';
+import Memo from '../models/Memo';
 
 import axios, { AxiosResponse } from 'axios';
 
@@ -42,7 +45,7 @@ const getEachUserBojInfo = async (bojId: string) => {
 };
 
 /* Recreate response based on info through solved.ac api */
-const regenerateData = async (datum: DatumType[]) => {
+const regenerateData = async (datum: any) => {
   for await (const data of datum) {
     // console.log(data);
     const eachData: false | AxiosResponse<any, any> = await getEachUserBojInfo(
@@ -64,31 +67,34 @@ const regenerateData = async (datum: DatumType[]) => {
 
 /* Get all user's number of solved problems through boj ids in DB */
 export const getUsersBojInfo = async (req: Request, res: Response) => {
-  const uri = `mongodb+srv://juncheol:${mongoPassword}@cluster0.v0izvl3.mongodb.net/?retryWrites=true&w=majority`;
-  const client = new MongoClient(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  // const uri = `mongodb+srv://juncheol:${mongoPassword}@cluster0.v0izvl3.mongodb.net/?retryWrites=true&w=majority`;
+  // const client = new MongoClient(uri, {
+  //   useNewUrlParser: true,
+  //   useUnifiedTopology: true,
+  // });
 
   /* Connect to DB */
-  const dbconnect = async () => {
-    try {
-      await client.connect();
-      console.log('Connected to DB to get boj infos from all users');
-    } catch (e) {
-      console.error(e);
-    }
-  };
+  // const dbconnect = async () => {
+  //   try {
+  //     await client.connect();
+  //     console.log('Connected to DB to get boj infos from all users');
+  //   } catch (e) {
+  //     console.error(e);
+  //   }
+  // };
 
-  await dbconnect();
+  // await dbconnect();
 
-  const db = client.db('codewart');
-  const collection = db.collection('users');
+  // const db = mongoClient.db('codewart');
+  // const collection = db.collection('users');
+
+  const datum = await User.find({});
+  console.log(datum);
 
   /* Get all users' datum in the DB */
-  const datum = await collection
-    .find({}, { _id: false, userBojId: true, userNickname: true, userId: true })
-    .toArray();
+  // const datum = await collection
+  //   .find({}, { _id: false, userBojId: true, userNickname: true, userId: true })
+  //   .toArray();
 
   await regenerateData(datum);
 
@@ -107,3 +113,16 @@ export const getUsersBojInfo = async (req: Request, res: Response) => {
   /* Empty data */
   response = [];
 };
+
+/* DB에 메모 등록 */
+export const fetchMemo = async (req: Request, res: Response) => {
+  // const db = mongoClient.db('codewarts');
+  // const collection = db.collection('memos');
+  // collection.insert({});
+};
+
+/* DB에서 메모 불러오기 */
+export const getMemo = async () => {};
+
+/* 메모 수정하기 */
+export const changeMemo = async () => {};
