3bfc42e52ee28ae086c3e2d52eb3e3c20a84f233
100mgml@gmail.com <100mgml@gmail.com>
2023-02-28 22:21:34 +0900

    feat : Implement scraping solvedAC

--- server/servers/scrapingserver/updating_bojdb.py
+++ server/servers/scrapingserver/updating_bojdb.py
@@ -2,51 +2,54 @@ import requests
 from bs4 import BeautifulSoup
 import json
 from pymongo import MongoClient
+import time
 
-client = MongoClient('mongodb+srv://juncheol:0000@cluster0.v0izvl3.mongodb.net/?retryWrites=true&w=majority')
+client = MongoClient(
+    'mongodb+srv://juncheol:0000@cluster0.v0izvl3.mongodb.net/?retryWrites=true&w=majority')
 db = client.codewart
 collection = db.probs
 
 # 백준 서버에 요청
-headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-s = 1000    # 시작문제넘버
-e = 1001    # 종료문제넘버
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+s = 3001    # 시작문제넘버
+e = 3700    # 종료문제넘버
+
+miss = []
 
 for num in range(s, e+1):
-    probId = num
-    data = requests.get(f'https://www.acmicpc.net/problem/{probId}',headers=headers)
-    soup = BeautifulSoup(data.text, 'html.parser')
+    data2 = requests.get(
+        f'https://solved.ac/api/v3/problem/show?problemId={num}', headers=headers)
 
-    try:
-        prob_desc = soup.select_one('#problem_description').get_text()
-    except:
+    if (data2.status_code != 200):
+        miss.append(num)
+        print(num, ":", "something is wrong")
         continue
-    try: 
-        source = soup.select_one('#source > p > a:nth-child(2)').get_text()
-    except: 
-        source = ''
 
-    samples = {}
-    for i in range(1, 50):
-        inputval = soup.select_one(f'#sample-input-{i}')
-        if (inputval == None):
-            break
-        else:
-            outputval = soup.select_one(f'#sample-output-{i}')
-            samples[str(i)] = {"input": inputval.get_text(), "output":outputval.get_text()}
-            i += 1
+    # Convert byte string to regular string
+    data2_dict = json.loads(data2.text)
 
-    crawled_data = {
-    "probId":probId,
-    "prob_desc" : prob_desc,
-    "prob_input" : prob_input,
-    "prob_output" : prob_output,
-    "samples" : samples,
-    "source" : source
-    }
+    # define the filter for the document to update
+    filters = {"probId": num}
 
-    # DB에 삽입하기
-    collection.insert_one(crawled_data)
+    # define the update operation
+    update = {"$set": {"solvedAC": data2_dict}}
+
+    # DB에 업데이트하기
+    result = collection.update_one(filters, update)
+
+    # print the result of the update operation
+    print(num, ":", result.modified_count)
+    time.sleep(0.3)
 
 
+print(miss)
 
+# crawled_data = {
+# "probId":probId,
+# "prob_desc" : prob_desc,
+# "prob_input" : prob_input,
+# "prob_output" : prob_output,
+# "samples" : samples,
+# "source" : source
+# }
