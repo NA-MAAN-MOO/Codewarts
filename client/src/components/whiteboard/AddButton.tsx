0d7f9631bb36c4a67878bc567f99dbcb40dd11fe
HyejiYeom <kristi8041@gmail.com>
2023-03-01 12:02:49 +0900

    style : Change add button style for memos

--- client/src/components/whiteboard/AddButton.tsx
+++ client/src/components/whiteboard/AddButton.tsx
@@ -1,43 +1,21 @@
-import { Add } from '@mui/icons-material';
-import { IconButton } from '@mui/material';
-import { AddBox } from '@mui/icons-material';
+import Fab from '@mui/material/Fab';
+import AddIcon from '@mui/icons-material/Add';
 import styled from 'styled-components';
-import { RootState } from 'stores';
-import { useSelector } from 'react-redux';
-import { useEffect, useMemo } from 'react';
-import axios from 'axios';
 
 export default function AddButton(props: any) {
-  const currentUserId = useSelector((state: RootState) => state.user.playerId);
-  const currentUserNickname = useSelector(
-    (state: RootState) => state.user.playerNickname
-  );
-
-  const initialMemoModel = {
-    authorId: currentUserId,
-    authorNickname: currentUserNickname,
-    content: '',
-    x: 80,
-    y: 80,
-    participants: [],
-  };
-
-  //TODO: 메모 추가하면 프레임 하나 생기고, 리덕스에 저장해두었다가, onChange시 DB에 저장
-  const addMemo = () => {};
-  // const { addMemo } = props;
+  const { addMemo } = props;
 
   return (
     <AddBtnWrapper>
-      <IconButton
-        aria-label="close"
+      <Fab
         size="large"
-        //TODO: addMemo 구현하기
-        //   onClick={addMemo}
         color="secondary"
-        sx={{ float: 'right', marginTop: '10px', marginRight: '20px' }}
+        aria-label="add"
+        onClick={addMemo}
+        sx={{ float: 'right', marginRight: '20px' }}
       >
-        <AddBox />
-      </IconButton>
+        <AddIcon />
+      </Fab>
     </AddBtnWrapper>
   );
 }
