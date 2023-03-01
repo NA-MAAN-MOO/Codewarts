6ac303aab18ed8c88e892c07667d22c7755be5c7
HyejiYeom <kristi8041@gmail.com>
2023-03-01 12:09:42 +0900

    feat : Implement rendering all memos in DB

--- client/src/components/whiteboard/MemoDump.tsx
+++ client/src/components/whiteboard/MemoDump.tsx
@@ -1,5 +1,38 @@
+import React from 'react';
 import Memo from './Memo';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
-export default function MemoDump(props: any) {
-  return <Memo />;
+function MemoDump(props: any) {
+  //FIXME: 이걸 redux로 전역적으로 관리할 수 있게 바꿔야 할듯?
+  const {
+    memos,
+    addMemo,
+    getMemos,
+    deleteMemo,
+    updateMemo,
+    changeMemoPos,
+    currentUserNickname,
+  } = props;
+
+  return (
+    <>
+      {memos.length !== 0 ? (
+        memos.map((memo: {}) => (
+          <Memo
+            memo={memo}
+            getMemos={getMemos}
+            deleteMemo={deleteMemo}
+            updateMemo={updateMemo}
+            changeMemoPos={changeMemoPos}
+            currentUserNickname={currentUserNickname}
+          />
+        ))
+      ) : (
+        <div></div>
+      )}
+    </>
+  );
 }
+
+export default React.memo(MemoDump);
