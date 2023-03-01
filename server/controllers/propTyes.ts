919fbc355898750c020d26d488d83f6f5f857894
100mgml@gmail.com <100mgml@gmail.com>
2023-03-01 16:57:41 +0900

    fix : Make prob level filtering work

--- server/controllers/propTyes.ts
+++ server/controllers/propTyes.ts
@@ -17,4 +17,21 @@ export interface IProbInfo {
   prob_output?: string;
   samples?: { input: string; output: string }[];
   source?: string;
+  solvedAC?: {
+    problemId: number;
+    titleKo: string;
+    titles: [];
+    isSolvable: boolean;
+    isPartial: boolean;
+    acceptedUserCount: number;
+    level: number;
+    votedUserCount: number;
+    sprout: boolean;
+    givesNoRating: boolean;
+    isLevelLocked: boolean;
+    averageTries: number;
+    official: boolean;
+    tags: object;
+    metadata: object;
+  };
 }
