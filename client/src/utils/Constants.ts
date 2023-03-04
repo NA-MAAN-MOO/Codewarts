d0f6f8a8c656ec9dad6560a7121678041276137c
김세희 <sadie1001@naver.com>
2023-03-04 16:24:00 +0900

    feat : make URL constant

--- client/src/utils/Constants.ts
+++ client/src/utils/Constants.ts
@@ -18,3 +18,17 @@ export const VOICE_STATUS = {
   COMPLETE: 'COMPLETE',
   FAIL: 'FAIL',
 };
+
+export const APPLICATION_URL = {
+  APPLICATION_VOICE_URL: process.env.REACT_APP_SERVER_URL
+    ? `${process.env.REACT_APP_SERVER_URL}/voice`
+    : 'http://localhost:3002',
+  APPLICATION_DB_URL: process.env.REACT_APP_DB_URL || 'http://localhost:3003',
+  APPLICATION_EDITOR_URL:
+    process.env.REACT_APP_EDITOR_URL || 'http://localhost:3001',
+  APPLICATION_SERVER_URL:
+    process.env.REACT_APP_SERVER_URL || 'http://localhost:8080',
+  APPLICATION_BOARD_URL:
+    process.env.REACT_APP_SERVER_URL || 'http://localhost:3004',
+  APPLICATION_YJS_URL: process.env.REACT_APP_YJS_URL || 'ws://localhost:1234/',
+};
