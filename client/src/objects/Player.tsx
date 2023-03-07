3b930ac1d09be632a7403805ea4183ddb5ac5dfb
HyejiYeom <kristi8041@gmail.com>
2023-03-07 16:02:24 +0900

    fix : Destroy success effect when player exits

--- client/src/objects/Player.tsx
+++ client/src/objects/Player.tsx
@@ -205,6 +205,14 @@ export default class Player extends Phaser.Physics.Matter.Sprite {
     }, 120000);
   }
 
+  /* When player close editor or exits, 강제적으로 성공 이펙트 없애버림 */
+  //FIXME:
+  destroySuccessEffect() {
+    if (this.successEffect) {
+      this.successEffect.destroy();
+    }
+  }
+
   //emoji
 
   updateDialogBubble(content: string) {
