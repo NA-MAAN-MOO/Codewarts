54557e9a3b295cf0f2834dbe2df99f2de79de09a
mrsuit0114 <woosa0114@gmail.com>
2023-03-07 23:00:43 +0900

    chore : modify delay dialog fade

--- client/src/components/LoginDialog.tsx
+++ client/src/components/LoginDialog.tsx
@@ -159,7 +159,7 @@ const LoginDialog = () => {
 
   return (
     <Wrapper
-      className="animate__animated animate__fadeIn animate__delay-2s"
+      className="animate__animated animate__fadeIn animate__delay-0.8s"
       style={{
         position: 'absolute',
         backgroundImage: `url(${loginFrame})`,
