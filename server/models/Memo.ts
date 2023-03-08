9c301c04a104816ba2a4f3dd2333e3ede0801897
김준철 <kimjoon889@gamil.com>
2023-03-08 17:49:13 +0900

    fix : set time zone

--- server/models/Memo.ts
+++ server/models/Memo.ts
@@ -1,14 +1,22 @@
 import { Schema, model, Document, Model, Types } from 'mongoose';
+require('moment-timezone');
+var moment = require('moment');
+moment.tz.setDefault('Asia/Seoul');
 
 export function getDate(): string {
-  let dateNow = new Date();
-
-  let fullYear = String(dateNow.getFullYear());
+  // let dateNow = new Date();
+  // let fullYear = String(dateNow.getFullYear());
+  // let year = fullYear.substring(2, 4);
+  // let month = String(dateNow.getMonth() + 1);
+  // let day = String(dateNow.getDate());
+  // let hour = String(dateNow.getHours());
+  // let minute = String(dateNow.getMinutes());
+  let fullYear = String(moment().years());
   let year = fullYear.substring(2, 4);
-  let month = String(dateNow.getMonth() + 1);
-  let day = String(dateNow.getDate());
-  let hour = String(dateNow.getHours());
-  let minute = String(dateNow.getMinutes());
+  let month = String(moment().months() + 1);
+  let day = String(moment().date());
+  let hour = String(moment().hour());
+  let minute = String(moment().minute());
   return year + '.' + month + '.' + day + '. ' + hour + ':' + minute;
 }
 
